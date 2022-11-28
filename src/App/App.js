4c5d37e847d8c218f55d1097064121cfeaf55d96
Shivani <shivanikumari@joshtalks.com>
2022-11-29 03:03:50 +0530

    contents

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Cart from '../Pages/Cart/Cart';
+import Home from '../Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-              <Cart/>
+              {/* <Cart/> */}
+              <Home/>
     </div>
   );
 }
