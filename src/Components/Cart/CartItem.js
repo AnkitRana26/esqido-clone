3c04c6c29e7bfa18e3cf9d90ac0f0dbdef4a8a39
ankitrana <ranaankitr@outlook.com>
2022-12-01 02:33:24 +0530

    skeleton Added

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,7 +1,7 @@
-import { Box, Button, Container, Image, Text, useMediaQuery } from '@chakra-ui/react'
+import { Box, Button, Center, Container, Image, Text, useMediaQuery } from '@chakra-ui/react'
 import { FaShoppingCart } from "react-icons/fa";
 import React, { useEffect, useState } from 'react'
-import { FormControl,InputLabel, MenuItem, Select } from '@mui/material';
+import { FormControl,InputLabel, MenuItem, Select, Skeleton } from '@mui/material';
 import { GiCancel } from "react-icons/gi";
 import { BsFillCartFill,BsCurrencyDollar,BsFillCartCheckFill,BsFillPatchCheckFill } from "react-icons/bs";
 import { AiOutlineShoppingCart,AiOutlineDollarCircle,AiFillDelete } from "react-icons/ai";
@@ -50,6 +50,13 @@ const CartItem = () => {
   // const [value,setValue] = useState('');
   const [cart,setCart] = useState(cartData);
   const [isTablet] = useMediaQuery("(max-width: 900px)") 
+  const [isLoading,setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  },[])
 
 
   const handleChange =(id,value)=>{
@@ -112,7 +119,17 @@ const CartItem = () => {
         <Box p="2% 2%" display="grid" alignItems="center" fontSize="large" color="#58595B" fontWeight="semibold" border="1px solid rgba(0,0,0,.15)"   >
         {
           cart.map(ele=>{
-            return <Box key={ele.id} p="1%" m="1%"  display="grid" gridTemplateColumns={isTablet?"repeat(1,1fr)":"25% 75%"}   gap="1%" borderBottom="1px solid rgba(0,0,0,.15)" >
+            return isLoading?<div style={{marginBottom:"1%",padding:"1%",display:"flex",justifyContent:"space-between",alignItems:"center" }} >
+              
+              <Skeleton variant="rectangular" width={"120px"} height={"120px"}/>
+              <Skeleton variant="rectangular" width={"300px"} height={"40px"}/>
+              <div style={{display:"flex",gap:"10px",justifyContent:"space-between",alignItems:"center" }} >
+              <Skeleton variant="rectangular" width={"80px"} height={"50px"}/>
+              <Skeleton variant="text" width={"80px"} height={"50px"}/>
+
+              </div>
+
+            </div> :<Box key={ele.id} p="1%" m="1%"  display="grid" gridTemplateColumns={isTablet?"repeat(1,1fr)":"25% 75%"}   gap="1%" borderBottom="1px solid rgba(0,0,0,.15)" >
                     <Image h="120px" w="120px" margin={isTablet?"auto":""} src={ele.img} />
                     <Box p="1%" w="100%" display="grid" gridTemplateColumns={isTablet?"repeat(1,1fr)":"65% 35%"}  alignItems="center"  justifyContent="space-between" >
                           <Box h="fit-content" textAlign={isTablet?"center":"left"} >
